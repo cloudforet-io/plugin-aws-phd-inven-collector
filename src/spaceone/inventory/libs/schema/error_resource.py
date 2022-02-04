bde5cf38310d9f5540098b7c2c78c75001b4d846
stat_kwon <stat_kwon@naver.com>
2022-02-04 09:10:15 +0900

    add schema about error resource

---
+++ src/spaceone/inventory/libs/schema/error_resource.py
@@ -0,0 +1,16 @@
+from schematics import Model
+from schematics.types import StringType, ModelType
+from .base import BaseResponse
+
+class ErrorResource(Model):
+    resource_type = StringType(default='inventory.CloudService')
+    provider = StringType(default='aws')
+    cloud_service_group = StringType(serialize_when_none=False)
+    cloud_service_type = StringType(serialize_when_none=False)
+    resource_id = StringType(serialize_when_none=False)
+
+
+class ErrorResourceResponse(BaseResponse):
+    state = StringType(default='FAILURE')
+    resource_type = StringType(default='inventory.ErrorResource')
+    resource = ModelType(ErrorResource, default={})
